0c34dcbfd8613173be96b7fba67b832feee8019a
samiras <samiras@localhost>
2023-11-04 23:06:18 -0400

    11.02.23 commit

--- mediNote/src/main/java/com/medinote/medinote/controller/NoteController.java
+++ mediNote/src/main/java/com/medinote/medinote/controller/NoteController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +27,6 @@ import jakarta.validation.Valid;
  * that handles HTTP requests related to note information.
  */
 @RestController
-
 
 public class NoteController {
     /**
@@ -119,7 +117,5 @@ public class NoteController {
         log.info("get note by id :{} ", id);
         return new ResponseEntity<>(noteService.findNoteByPatientId(id), OK);
     }
-
-
 
 }
